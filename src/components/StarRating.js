ba0f59c83e023b9396ff4f9f0a0727406a5a559d
Masha Blair <mashablair@gmail.com>
2019-02-26 12:59:49 -0800

    Add a function that renders 5 stars.

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,9 +4,24 @@ import Star from './Star';
 class StarRating extends Component {
 
   // Initialize a 'rating' state
+  state = {
+    rating: 0
+  }
 
 
   // Write a function that returns 5 Star components
+  renderStars = () => {
+    let stars = [];
+    let maxRating = 5;
+    for (let i = 0; i < maxRating; i++) {
+      stars.push(
+        <Star
+          key={i}
+        />
+      );
+    }
+    return stars;
+  }
 
 
   // Write an event handler that updates the rating state.
@@ -17,6 +32,7 @@ class StarRating extends Component {
     return (
       <ul className="course--stars">
         {/* Render the Star components */}
+        {this.renderStars()}
       </ul>
     );
   }
